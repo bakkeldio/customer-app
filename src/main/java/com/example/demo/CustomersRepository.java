07dbdba78a65d4c3379c302757e22286c123c087
Bakkeldi <bakkeldi.orozbekov00@gmail.com>
2020-04-03 15:27:16 +0600

    First Commit

---
+++ src/main/java/com/example/demo/CustomersRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface CustomersRepository extends JpaRepository<Customers, Long> {
+    List<Customers> findAll();
+}
