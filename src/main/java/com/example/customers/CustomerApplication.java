54664cd50e6fa450ba51dd6b9c566c5c79edec12
Bakkeldi <bakkeldi.orozbekov00@gmail.com>
2020-04-14 15:07:38 +0600

    Additional Commit

--- src/main/java/com/example/customers/CustomerApplication.java
+++ src/main/java/com/example/customers/CustomerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerServiceApplication {
+public class CustomerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CustomerServiceApplication.class, args);
+        SpringApplication.run(CustomerApplication.class, args);
     }
 }
--- src/main/java/com/example/customers/ServletInitializer.java
+++ src/main/java/com/example/customers/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(CustomerServiceApplication.class);
+        return application.sources(CustomerApplication.class);
     }
 
 }
